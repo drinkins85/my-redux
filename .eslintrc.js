169ebaefa323f4d472a39f412b9cc7e4b77888a0
Andre <drinkins85@mail.ru>
2020-11-23 13:36:23 +0300

    eslint switch indent rule fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -34,7 +34,7 @@ module.exports = {
         ],
     },
     rules: {
-        indent: [2, 4],
+        indent: [2, 4, { SwitchCase: 1 }],
         'react/jsx-indent': [2, 4],
         'react/jsx-indent-props': [2, 4],
         'react/jsx-filename-extension': [2, {
